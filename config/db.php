f1ae88b12857d95492c3b22fa83c2e4aeaf695d2
Dmytro Orikhovskyi <dimaoag@gmail.com>
2018-01-12 12:08:17 +0200

    12-01-2018 12:08

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=mysite.loc;dbname=mysite',
     'username' => 'root',
-    'password' => '',
+    'password' => '1111',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
